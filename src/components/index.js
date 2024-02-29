082fc8be50732c950cad92a3adb0fcf97fa392ee
mmj030703 <mayankjain7738@gmail.com>
2024-02-29 20:31:22 +0530

    Created some more components.

--- src/components/index.js
+++ src/components/index.js
@@ -5,6 +5,11 @@ import Container from './container/container';
 import Logout from './Header/Logout';
 import Button from './Header/Button';
 import Input from './Input';
+import Select from "./Select";
+import PostCard from './PostCard';
+import Login from "./Login";
+import Signup from "./Signup";
+import AuthLayout from "./AuthLayout";
 
 export {
     Header,
@@ -13,5 +18,10 @@ export {
     Container,
     Logout,
     Button,
-    Input
+    Input,
+    Select,
+    PostCard,
+    Login,
+    Signup,
+    AuthLayout
 }
